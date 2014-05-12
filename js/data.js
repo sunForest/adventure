f6619d0d4960e9ee6984249c7004eee2d470fbc9
sunForest <sensun@live.cn>
2014-05-12 22:42:45 +0200

    first commit

---
+++ js/data.js
@@ -0,0 +1,64 @@
+var geojson = {
+    "type": "FeatureCollection",
+    "features": [
+        { 
+            "type": "Feature",
+            "geometry": {
+                "type": "Point", 
+                "coordinates": [40, 36]
+            },
+            "properties": {
+                "name": "cover",
+                "zoom": [3, 3]
+            }
+        },
+        { 
+            "type": "Feature",
+            "geometry": {
+                "type": "Point", 
+                "coordinates": [121.979603, 39.627114]
+            },
+            "properties": {
+                "name": "Wafangdian",
+                "marker": "building",
+                "zoom": [3, 9]
+            }
+        },
+        { 
+            "type": "Feature",
+            "geometry": {
+                "type": "Point", 
+                "coordinates": [121.614682, 38.914003]
+            },
+            "properties": {
+                "name": "Dalian",
+                "marker": "school",
+                "zoom": [8, 11]
+            }
+        },
+        { 
+            "type": "Feature",
+            "geometry": {
+                "type": "Point", 
+                "coordinates": [116.3041381, 39.9919223]
+            },
+            "properties": {
+                "name": "Peking University",
+                "marker": "college",
+                "zoom": [6, 9]
+            }
+        },
+        { 
+            "type": "Feature",
+            "geometry": {
+                "type": "Point", 
+                "coordinates": [8.667611899, 49.4178816]
+            },
+            "properties": {
+                "name": "Heidelberg University",
+                "marker": "college",
+                "zoom": [4, 9]
+            }
+        }
+    ]
+};
